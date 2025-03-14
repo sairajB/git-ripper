8f99865c130abfa2a9a9b7ebc94b14b39abfca3d
sairajB <sairaj.sab@gmail.com>
2025-03-14 13:29:36 +0530

    Changed the version name

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { downloadFolder } from './downloader.js';
 
 const initializeCLI = () => {
   program
-    .version('1.1.2')
+    .version('1.2.1')
     .description('Clone specific folders from GitHub repositories')
     .argument('<url>', 'GitHub URL of the folder to clone')
     .option('-o, --output <directory>', 'Output directory', process.cwd())
